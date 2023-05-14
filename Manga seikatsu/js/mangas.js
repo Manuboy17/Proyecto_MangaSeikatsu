ae97b36fff29161c8f45c35a7434351a193c37a0
Manuboy17 <107696896+Manuboy17@users.noreply.github.com>
2023-05-13 22:45:48 -0400

    automatizamos las cards con el json

---
+++ Manga seikatsu/js/mangas.js
@@ -0,0 +1,45 @@
+fetch('anime.json')
+    .then(res => res.json())
+    .then(data => {
+        const contenedor = document.querySelector('#card-container');
+        data.forEach(producto => {
+            const cardHTML = crearCard(producto);
+            contenedor.insertAdjacentHTML('beforeend', cardHTML);
+        });
+    })
+function crearCard(data) {
+  return `
+    <div class="col mb-4">
+        <div class="product-grid card h-100 border-light mb-3">
+            <div class="product-image">
+                <a href="#" class="image">
+                <img class="pic-1"
+                    src="${data.pic1}"
+                />
+                <img class="pic-2" 
+                    src="${data.pic2}" />
+                </a>
+                <ul class="product-links">
+                    <li>
+                        <a href="#"><i class="fa fa-shopping-cart"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="far fa-heart"></i></a>
+                    </li>
+                </ul>
+            </div>
+            <div class="product-content card-body">
+                <ul class="rating">
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="far fa-star"></li>
+                </ul>
+                <h5 class="card-title">${data.titulo}</h5>
+                <div class="price">$${data.precio} </div>
+            </div>
+        </div>
+    </div>
+   `
+}
