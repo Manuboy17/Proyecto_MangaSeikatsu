f0d1782c67219c5f92803ac0ef3e4117a3f5dcfc
Manuboy17 <107696896+Manuboy17@users.noreply.github.com>
2023-06-27 18:35:22 -0400

    2.5.5.2

--- Manga seikatsu/js/soloCompra.js
+++
@@ -1,6 +0,0 @@
-const btnCarrito = document.getElementById("btn-compra")
-const contManga = document.getElementById("cont-manga")
-
-btnCarrito.addEventListener("click", () => {
-    console.log('jholas')
-})
